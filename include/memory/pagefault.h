62ac4b6eada36d441c1c38a30a346342a71bd6a1
DevNugget <dominicthemiya@gmail.com>
2026-02-12 10:33:19 +0530

    Nuked

--- include/memory/pagefault.h
+++
@@ -1,9 +0,0 @@
-#ifndef PAGEFAULT_H
-#define PAGEFAULT_H
-
-#include <stdint.h>
-#include <com1.h>
-
-void pagefault_handler(uint64_t error_code, uint64_t rip);
-
-#endif 
--- userspace/libc/heap.h
+++
@@ -1,9 +0,0 @@
-#ifndef HEAP_H
-#define HEAP_H
-
-#include "stdint.h"
-
-void* malloc(uint64_t size);
-void free(void* ptr);
-
-#endif
